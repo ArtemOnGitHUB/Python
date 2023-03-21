3a661f1b33aee7b112c213808860c284721857a6
Artem <Kuzmichev8-1991@list.ru>
2023-03-21 20:30:31 +0300

    from the begin

---
+++ Lection/Lesson_1/5.py
@@ -0,0 +1,85 @@
+# a = 5
+# d = 6
+# c = "gtrrr"
+# print(a,d,c)
+
+# a = 5
+# d = 6
+# c = "gtrrr"
+# print(f"{a} - {d} - {c}")
+
+# a = 5
+# d = 6
+# c = "gtrrr"
+# print("{} - {} - {}".format(a,d,c))
+
+# print("Ensert first str")
+# a = input()
+# print(a)
+
+# print("insert first number")
+# a = input()
+# b = input("insert second number: ")
+
+# print("insert first number")
+# a = input()
+# b = input("insert second number: ")
+
+# print(a,' + ', b, ' = ', a + b)
+
+# c = 5.89
+# print(c)
+# print(type(c))
+# c = str(c)
+# print(c + '89')
+# print(type(c))
+
+# c = 1
+# print(c)
+# print(type(c))
+# c = bool(c)
+# print(c)
+# print(type(c))
+
+# v = 'fdffd'
+# v = int(v)
+
+# print("insert first number")
+# a = int(input())
+# b = int(input("insert second number: "))
+
+# print(a,' // ', b, ' = ', a // b)
+
+# a = 5.64464
+# b = 5.66666
+# print(round (a*b, 3))
+
+# a = 1 > 4
+# print(a)
+# a = 1 < 4 and 5 > 2
+# print(a)
+# a = 1 != 2
+# print(a)
+# a = 'qwe'
+# b = 'qwe'
+# print(a == b)
+
+# username = input('Input name: ')
+# if username == 'Mary':
+#     print('Its Mary')
+# elif username == 'Marina':
+#     print('Its Marina')
+# elif username == 'Ilnar':
+#     print('Ilnar good')
+# else:
+#     print('Hello,', username)
+
+i = 0
+while i < 5:
+    if i == 3:
+        break
+    i = i + i
+else:
+    print('Its')
+    print('Enough')
+print(i)
